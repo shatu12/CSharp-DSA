c2bca54f8a3526873bb440f27b467fa65ec0579e
shatu12 <rokeyaafrozshatu@gmail.com>
2026-07-24 13:21:03 +0600

    add run files

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// Console.WriteLine("Hello, World!");
 
-var solver = new TwoSumSolution();
-int[] result = solver.TwoSum(new int[] {2,7,11,15}, 9);
-Console.WriteLine($"Result: [{result[0]}, {result[1]}]");
+var solver = new ValidAnagramSolution();
+bool result = solver.IsValidSolution("silent", "isten");
+Console.WriteLine($"Result: {result}");
